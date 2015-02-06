77b25b51032761a7aba6150da4984817c1456538
Eric Rosenberg <ehaydenr@gmail.com>
2015-02-05 20:17:20 -0600

    Basic Listing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,12 @@
 class PagesController < ApplicationController
-    def home
-    end
+
+  def home
+    @comments = Comment.all
+    puts "Hello"
+  end
+
+  def home_render
+    render :home
+  end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+c0 = Comment.create(content: "Because he loves us.")
